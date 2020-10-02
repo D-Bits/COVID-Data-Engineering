02df3aa6d90e19d292ce17ce7112f622fb07e4c9
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-02 15:57:56 -0700

    Added HTML bodies for alert emails

---
+++ dags/emails/alert_bodies.py
@@ -0,0 +1,14 @@
+
+cases_body = """
+
+<p>As of today, the total number of COVID-19 cases in
+the United States has exceeded 7 million.</p>
+
+"""
+
+deaths_body = """
+
+<p>As of today, the total number of COVID-19 deaths in
+the United States has exceeded 2 million.</p>
+
+"""
