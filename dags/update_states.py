ea04e06bd3b21c6ef9df1ba0c4e4bb4a994864fe
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-04 01:02:33 -0700

    Added update_states DAG.

---
+++ dags/update_states.py
@@ -0,0 +1,85 @@
+"""
+DAG to update current data for U.S. States and territories
+"""
+from os import getenv
+from requests import get
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import pandas as pd 
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2020, 10, 1),
+    "retries": 1,
+}
+
+dag = DAG('UpdateStates', default_args=default_args, schedule_interval="@daily")
+
+def extract_transform(**context):
+
+    data = get("https://api.covidtracking.com/v1/states/current.json").json()
+
+    df = pd.DataFrame(data, index=None).drop([
+        'deathConfirmed',
+        'deathProbable',
+        'totalTestEncountersViral',
+        'totalTestsPeopleViral',
+        'totalTestsAntibody',
+        'positiveTestsAntibody',
+        'negativeTestsAntibody',
+        'totalTestsPeopleAntibody',
+        'positiveTestsPeopleAntibody',
+        'negativeTestsPeopleAntibody',
+        'totalTestsPeopleAntigen',
+        'positiveTestsPeopleAntigen',	
+        'totalTestsAntigen',	
+        'positiveTestsAntigen',
+        'pending',
+        'totalTestResults',
+        'commercialScore',	
+        'negativeRegularScore',
+        'negativeScore',	
+        'positiveScore',	
+        'score',	
+        'grade',
+        'fips',
+        'totalTestResultsSource',
+        'onVentilatorCumulative',
+        'positiveTestsViral',
+        'negativeTestsViral',
+        'positiveCasesViral',
+        'total',
+        'hash'
+    ], axis=1)
+
+    # Create an XCOM for this task to be used in load()
+    context['ti'].xcom_push(key="df", value=df)
+
+
+def load(**context):
+
+    # Fetch the cleaned DataFrame from the above XCOM
+    df = context["ti"].xcom_pull(key="df")
+
+    # Fetch SQL Alchemy connection string from .env file
+    db_conn = getenv("SQL_ALCHEMY_CONN")
+    # Dump df to csv, and then load into db
+    df.to_sql(
+        'states_current', 
+        db_conn, 
+        index=False, 
+        schema='covid', 
+        method='multi', 
+        if_exists='replace'
+    )
+
+
+with dag:
+
+    t1 = PythonOperator('extract_transform', python_callable=extract_transform, provide_context=True)
+    t2 = PythonOperator('load', python_callable=load, provide_context=True)
+
+
+    t1 >> t2
