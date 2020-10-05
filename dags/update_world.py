789eb77970d9f46e28ac387c7c0b6ca953c2be6d
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-05 14:10:29 -0700

    Started work on global data.

--- dags/update_world.py
+++ dags/update_world.py
@@ -1,5 +1,5 @@
 """
-DAG to update global data
+DAG to update global data.
 """
 from os import getenv
 from datetime import datetime, timedelta
@@ -20,3 +20,40 @@ default_args = {
 dag = DAG("update_world", default_args=default_args, schedule_interval="@daily")
 
 
+def extract_transform(**context):
+
+    data = get("https://api.covid19api.com/summary").json()
+
+    df = pd.DataFrame(data["Countries"], index=None)
+    # Clean data
+    cleaned_data = df.drop([
+        "Slug",
+        "Premium"
+    ], axis=1)
+
+    # Create an XCOM for this task to be used in load()
+    context['ti'].xcom_push(key="df", value=cleaned_data)
+
+
+def load(**context):
+
+    df = context["ti"].xcom_pull(key="df")
+    # Fetch SQL Alchemy connection string from .env file
+    db_conn = getenv("SQL_ALCHEMY_CONN")
+    # Dump df to csv, and then load into db
+    df.to_sql(
+        'global_summary', 
+        db_conn, 
+        index=False, 
+        schema='world', 
+        method='multi', 
+        if_exists='replace'
+    )
+
+
+with dag:
+
+    t1 = PythonOperator(task_id="extract_transform", python_callable=extract_transform, provide_context=True)
+    t2 = PythonOperator(task_id="load", python_callable=load, provide_context=True)
+
+    t1 >> t2
