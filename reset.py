291a72bddc8ef7b41ba9a5614a9863584fb13040
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-08 18:59:46 -0700

    Misc changes.

--- reset.py
+++ reset.py
@@ -4,12 +4,9 @@ Script to setup project/dev environment, and fix certain Docker problems.
 from subprocess import run, CalledProcessError
 
 
-try:
-    # Clean things (including volumes) up before building
-    run(['docker', 'system', 'prune']).check_returncode()
-    run(['docker', 'volume', 'rm', 'us-covid-data_uscovid']).check_returncode()
-    run(['docker-compose', 'build']).check_returncode()
-    run(['docker-compose', 'up']).check_returncode()
-    
-except CalledProcessError:
-    print("ERROR: A command failed to execute. We apologize for the inconvience.")
+# Clean things (including volumes) up before building
+run(['docker', 'system', 'prune']).check_returncode()
+run(['docker', 'volume', 'rm', 'covid-data_covid']).check_returncode()
+run(['docker-compose', 'build']).check_returncode()
+run(['docker-compose', 'up']).check_returncode()
+
